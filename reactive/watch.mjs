ae5e5ef9c4c7da9392d79747d6f107aeac57b700
lvyuanxin <1498781418@qq.com>
2022-11-01 16:16:24 +0800

    feat: watch 支持 immediate

--- reactive/watch.mjs
+++ reactive/watch.mjs
@@ -1,6 +1,6 @@
 import { effect, reactive, traverse } from "./utils.mjs";
 
-function watch(source, cb) {
+function watch(source, cb, options = {}) {
   // watch 可以接受一个 getter 函数，用户可以指定 watch 依赖哪些响应式数据
   // 只有当这些数据变化时，才会触发回调函数执行
   let getter
@@ -11,17 +11,27 @@ function watch(source, cb) {
   } else {
     getter = () => traverse(source)
   }
+
+  const job = () => {
+    newValue = effectFn()
+    cb(newValue, oldValue)
+    oldValue = newValue
+  }
   const effectFn = effect(
     () => getter(),
-    { 
+    {
       lazy: true,
       scheduler() {
-        newValue = effectFn()
-        cb(newValue, oldValue)
-        oldValue = newValue
+        job()
       }
     }
   )
+
+  if (options.immediate) {
+    job()
+  } else {
+    oldValue = effectFn()
+  }
   oldValue = effectFn()
 }
 
@@ -31,6 +41,9 @@ watch(obj, (newValue, oldValue) => {
   // 如果是对象，那么新值和旧值应该是一样的，因为指向了同一个内存地址
   // console.log(`数据发生变化了, new: ${newValue.foo}, old: ${oldValue.foo}`)
   // console.log(`数据发生变化了, new: ${newValue.bar}, old: ${oldValue.bar}`)
+}, {
+  // immediate 属性表示传递给watch的函数是否立即执行
+  immediate: true
 })
 
 obj.foo = obj.foo + 3
