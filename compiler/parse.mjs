6a5cfeddc3553af7796702667a708518c5fc92a1
lvyuanxin <1498781418@qq.com>
2022-11-04 17:25:08 +0800

    feat: compiler

--- compiler/parse.mjs
+++ compiler/parse.mjs
@@ -1,1 +1,33 @@
-`<p>vue</p>`
+import { dump, parse, traverseNode } from './utils.mjs'
+
+const ast = parse(`<div><p>vue</p><p>react</p></div>`)
+
+// 节点转换函数
+function transformElement(node) {
+  if (node.type === 'Element' && node.tag === 'p') {
+    node.tag = 'h1'
+  }
+}
+// 内容转换函数
+function transformText(node) {
+  if (node.type === 'Text') {
+    node.content = node.content.repeat(2)
+  }
+}
+
+// AST 转换函数
+export function transform(ast) {
+  const context = {
+    nodeTransforms: [
+      transformElement,
+      transformText
+    ]
+  }
+
+  traverseNode(ast, context)
+  dump(ast)
+}
+
+// dump(ast)
+
+transform(ast)
--- compiler/utils.mjs
+++ compiler/utils.mjs
@@ -14,7 +14,7 @@ function isAlpha(char) {
 }
 
 // 接收模板字符串作为参数，并将模版切割为 token 返回
-function tokeniz(str) {
+export function tokeniz(str) {
   // 状态机的当前状态：初始状态
   let currentState = State.initial
   // 用于缓存字符, chars中只能存储字母，不能用来存储 < 或 >
@@ -52,7 +52,7 @@ function tokeniz(str) {
         } else if (char === '>') {
           currentState = State.initial
           tokens.push({
-            type: 'tag', 
+            type: 'tag',
             name: chars.join('')
           })
           chars.length = 0
@@ -99,3 +99,77 @@ function tokeniz(str) {
   return tokens
 }
 
+
+export function parse(str) {
+  const tokens = tokeniz(str)
+  // console.log(tokens)
+  const root = {
+    type: 'Root',
+    children: []
+  }
+  const elementStack = [root]
+  while (tokens.length) {
+    const parent = elementStack[elementStack.length - 1]
+    const t = tokens[0]
+    switch (t.type) {
+      case 'tag':
+        const elementNode = {
+          type: 'Element',
+          tag: t.name,
+          children: []
+        }
+        parent.children.push(elementNode)
+        elementStack.push(elementNode)
+        break
+      case 'text':
+        const textNode = {
+          type: 'Text',
+          content: t.content,
+        }
+        parent.children.push(textNode)
+        break
+      case 'tagEnd':
+        elementStack.pop()
+        break
+    }
+    tokens.shift()
+  }
+  return root
+}
+
+// 打印节点信息 
+export function dump(node, indent = 0) {
+  // console.log(node)
+  const type = node.type
+  const desc = node.type === 'Root'
+    ? ''
+    : node.type === 'Element'
+      ? node.tag
+      : node.content
+
+  console.log(`${'-'.repeat(indent)}${type}:  ${desc}`)
+
+  if (node.children) {
+    node.children.forEach(n => dump(n, indent + 2))
+  }
+}
+
+/**
+ * AST 遍历函数
+ * @param {*} ast 
+ * @param {Object} context 上下文
+ */
+export function traverseNode(ast, context) {
+  const currentAst = ast
+  const transforms = context.nodeTransforms
+  for (let i = 0; i < transforms.length; i++) {
+    transforms[i](currentAst, context)
+  }
+
+  const children = currentAst.children
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      traverseNode(children[i], context)
+    }
+  }
+}
