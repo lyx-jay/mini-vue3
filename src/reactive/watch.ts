8f9424e814386121dfe7d03dfa746909cda632ac
mymLyx <1498781418@qq.com>
2022-11-26 08:32:40 +0000

    feat: watch 静态问题

--- src/reactive/watch.ts
+++ src/reactive/watch.ts
@@ -15,14 +15,23 @@ type Options = {
   flush?: 'post' | 'pre'
 }
 
+/**
+ * newValue: 新值
+ * oldValue: 旧值
+ * onInvaliadate: 过期回调函数
+ */
+type Callback = (newValue?: any, oldValue?: any, onInvalidate?: Function) => void
+
 // 读取响应式数据时，会和副作用函数建立联系，但是当副作用函数的options中存在scheduler时，会执行scheduler函数
-export function watch(source: any, cb: Function, options: Options = {}) {
+export function watch(source: any, cb: Callback, options: Options = {}) {
 
   let getter: Function
   // 新值
   let newValue: any
   // 旧值
   let oldValue: any
+  // 用来存储用户注册的过期回调
+  let cleanup: Function
   // 判断source是不是函数，如果是函数，直接给getter
   // 如果不是，调用 traverser 函数，读取对象的每一个属性
   if (typeof source === 'function') {
@@ -31,11 +40,20 @@ export function watch(source: any, cb: Function, options: Options = {}) {
     getter = () => traverse(source)
   }
 
+
+  function onInvalidate(fn: Function) {
+    cleanup = fn
+  }
+
   const job = () => {
     // 执行 effectFn 获得最新的值
     newValue = effectFn()
+    // 在调用回调函数之前，调用过期函数
+    if (cleanup) {
+      cleanup()
+    }
     // 执行回调函数
-    cb(newValue, oldValue)
+    cb(newValue, oldValue, onInvalidate)
     // 将新值更新为旧值
     oldValue = newValue
   }
